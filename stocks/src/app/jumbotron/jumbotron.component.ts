43f263b4f038c544f4d4cf639175bf4f6b98f9f0
Andrew Gerst <gerst20051@gmail.com>
2020-05-06 23:30:39 -0400

    add day and pretty date formatting to jumbotron

--- stocks/src/app/jumbotron/jumbotron.component.ts
+++ stocks/src/app/jumbotron/jumbotron.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
   styleUrls: ['./jumbotron.component.css']
 })
 export class JumbotronComponent implements OnInit {
+  currentDate = new Date();
   day = 1;
-  date = 'Monday, June 15, 2015';
+  date = '';
 
-  constructor() {}
+  constructor() {
+    this.date = this.formatDate(this.currentDate);
+  }
 
   ngOnInit(): void {}
+
+  formatDate(date: Date): string {
+    return `${dayNames[date.getDay()]}, ${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+  }
+
+  getDateForDay(): Date {
+    const nextDate = new Date(this.currentDate);
+    nextDate.setDate(this.currentDate.getDate() + this.day - 1);
+    return nextDate;
+  }
+
+  nextDay() {
+    this.day++;
+    this.date = this.formatDate(this.getDateForDay());
+  }
 }
